abfab55c3bc25256dc37eeab4548c1400b6f00e3
somfish <samatumb@gmail.com>
2020-06-25 00:51:43 +0300

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Samat on 25.06.2020.
+//  Copyright © 2020 creamcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
