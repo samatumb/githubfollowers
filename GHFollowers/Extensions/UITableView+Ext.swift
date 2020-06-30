927d70eea216856fa0fb882f2571c91e6cd713bc
somfish <samatumb@gmail.com>
2020-07-01 02:37:06 +0300

    Final pass

---
+++ GHFollowers/Extensions/UITableView+Ext.swift
@@ -0,0 +1,20 @@
+//
+//  UITableView+Ext.swift
+//  GHFollowers
+//
+//  Created by Samat on 01.07.2020.
+//  Copyright © 2020 creamcode. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    func reloadDataOnMainThread() {
+        DispatchQueue.main.async { self.reloadData() }
+    }
+    
+    func removeExcessCells() {
+        tableFooterView = UIView(frame: .zero)
+    }
+}
